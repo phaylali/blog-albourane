5d28c31cd66968bbaa8b54efe0b0bf3f4f92b3da
Phaylali <elfailaliayoub@gmail.com>
2021-12-12 23:20:57 +0100

    02

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,74 @@
+import useDarkMode from "./useDarkMode";
 import Head from 'next/head'
 
 export default function Home() {
+  const [colorTheme, setTheme] = useDarkMode();
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 h-screen w-screen p-10  dark:bg-blueGrey900 dark:text-blueGrey200 bg-blueGrey200 text-blueGrey900">
       <Head>
-      <title>Albourane Blog - مدونة ألبوران</title>
+        <title>Albourane Blog - مدونة ألبوران</title>
         <meta name="description" content="Albourane Artisanal Fishing Cooperative in Ouedlaou, Morocco.
 تعاونية الصيد البحري التقليدي بوادلو المغرب" />
         <link rel="canonical" href="https://blog.albourane.com/" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-
+      
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
+        <h1 className="text-5xl font-bold">
           Welcome to{' '}
           <a className="text-blue-500" href="https://albourane.com">
-          Albourane's
+            Albourane's
           </a> Blog
         </h1>
-        <h1 className="text-6xl font-bold">
-        مرحبا في مدونة{' '}
+        <h1 className="text-5xl font-bold">
+          مرحبا في مدونة{' '}
           <a className="text-blue-500" href="https://albourane.com">
-          ألبوران
-          </a> 
+            ألبوران
+          </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-        Albourane Artisanal Fishing Cooperative in Ouedlaou, Morocco.
+          Albourane Artisanal Fishing Cooperative in Ouedlaou, Morocco.
           تعاونية الصيد البحري التقليدي بوادلو المغرب
         </p>
 
-        
+
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-between w-full h-24 border-t">
+      {colorTheme === "light" ? (
+        <svg
+          onClick={() => setTheme("light")}
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 m-8"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+          />
+        </svg>
+      ) : (
+        <svg
+          onClick={() => setTheme("dark")}
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6 m-8"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
+      )}
         <a
           className="flex items-center justify-center"
           href="https://omniversify.com"
@@ -41,8 +76,8 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/omniversify.webp" alt="Omniverisfy" className="h-4 ml-2" />
-        </a>
+          <img src="/omniversify.webp" alt="Omniverisfy" className="h-4 ml-2 m-8" />
+        </a> 
       </footer>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
 module.exports = {
+  
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'purple': '#3f3cbb',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#evebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+        'blueGrey900':'#263238',
+        'blueGrey200':'#B0BEC5',
+        'blue500':'#2196F3'
+  
+      },
+    },
+    
   },
   plugins: [],
 }
